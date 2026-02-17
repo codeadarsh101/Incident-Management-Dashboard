710d262abf85bf0bd3b130e8585e312417968d84
Adarsh Agrahari <adarshagrahari718@gmail.com>
2026-02-17 20:30:34 +0530

    add prod api url

--- frontend/src/api/incidentApi.jsx
+++ frontend/src/api/incidentApi.jsx
@@ -2,8 +2,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
+
 
 export const getIncidents = (params) => {
   const cleanParams = Object.fromEntries(
